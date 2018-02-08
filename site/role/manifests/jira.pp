0146ae1764c66718e566984c0a31a847796227d7
Ralf Bosz <ralf@bosz.com>
2018-02-08 10:24:47 +0100

    Add order to execution jira-role
    JDK should be present, else two runs are required.

--- site/role/manifests/jira.pp
+++ site/role/manifests/jira.pp
@@ -3,5 +3,6 @@ class role::jira {
   include profile::jdk8
   include profile::postgresql
   include profile::jira
+  
+  Class['profile::base'] -> Class['profile::jdk8'] -> Class['profile::postgresql'] -> Class['profile::jira']
 }
-
